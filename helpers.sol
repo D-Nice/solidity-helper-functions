d8d5ff7ba971e65b36a436cbfd393090877db281
D-Nice <D-Nice@users.noreply.github.com>
2016-03-17 19:44:51 -0400

    Create helpers.sol
    The helpers solidity file, you can import into existing projects.

---
+++ helpers.sol
@@ -0,0 +1,18 @@
+contract Helpers {
+    
+    //Sending function that is compatible with contracts as well. Using built-in send will normally cause inter-contract transactions to fail
+    function safeSend(address _receiver, uint _amtToSend) private {
+        if (_amtToSend > 0) {
+            bool success = _receiver.send(_amtToSend);
+            if (!success)
+    	        _receiver.call.value(_amtToSend); //May add .gas limit to set a max in case of parasitic contracts, see commented out method below 
+                //_receiver.call.value(_amtToSend).gas(maxGashere);
+	   }
+	}
+
+    //Checks if two strings are equal by comparing their hashes. Most efficient compare function, when amount of characters isn't large
+    //Have yet to test when you get diminishing returns on gas cost, but as character amount rises, other string compare functions become more efficient
+    function stringsEqual(string x, string y) returns (bool) {
+	    return sha3(x) == sha3(y) ? true : false;
+	}
+}
