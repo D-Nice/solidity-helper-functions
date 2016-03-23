14a515e7c1ece759b4a05b311371dcc2348d693d
D-Nice <D-Nice@users.noreply.github.com>
2016-03-23 17:22:34 -0400

    Added rounding helpers

--- helpers.sol
+++ helpers.sol
@@ -1,6 +1,7 @@
-contract Helpers {
+//By: D-Nice
+//Make a new contract if you commit any new helpers, using your name pre helpers.
+contract DNiceHelpers {
     
-    //By: D-Nice
     //Sending function that is compatible with contracts as well. Using built-in send will normally cause inter-contract transactions to fail
     function safeSend(address _receiver, uint _amtToSend) private {
         if (_amtToSend > 0) {
@@ -11,10 +12,38 @@ contract Helpers {
 	   }
 	}
 
-    //By: D-Nice
     //Checks if two strings are equal by comparing their hashes. Most efficient compare function, when amount of characters isn't large
     //Have yet to test when you get diminishing returns on gas cost, but as character amount rises, other string compare functions become more efficient
     function stringsEqual(string _a, string _b) returns (bool) {
 	    return sha3(_a) == sha3(_b) ? true : false;
 	}
+	
+	//The following is a combination of functions to recursively floor round an integer:
+	
+	//This constant function will convert a _number to be floored to the first _roundTo digits designated
+	//e.g. numberToRound(123456, 2) = 120000
+	function numberToRound(uint _number, uint _roundTo) constant returns (uint) {
+        return recursiveFloor(_number, _roundTo, findFigures(_number));
+    }
+    
+    function recursiveFloor(uint _number, uint _roundTo, uint _unit) private constant returns (uint) {
+        uint _rounded = _number / ((10**_unit) / 10);
+        if (_rounded >= 1 * ((10**_roundTo) / 10))
+            return _rounded * ((10**_unit) / 10);
+        else
+            return recursiveFloor(_number, _roundTo, _unit - 1);
+    }
+    
+    function findFigures(uint _number, uint _unit) private constant returns (uint) {
+        if (_number / (10**_unit / 10) < 10)
+            return _unit;
+        else
+            return findFigures(_number, _unit + 1);
+    }
+    
+    function findFigures(uint _number) private constant returns (uint) {
+        return findFigures(_number, 1);
+    }
+    //Ends here
+	
 }
