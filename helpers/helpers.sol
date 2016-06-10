39018634f0cf105ba6815a58d38b623d40bff497
D-Nice <D-Nice@users.noreply.github.com>
2016-06-10 14:12:45 -0400

    Update helpers.sol

--- helpers/helpers.sol
+++ helpers/helpers.sol
@@ -4,12 +4,9 @@ contract DNiceHelpers {
 
     //Sending function that is compatible with contracts as well. Using built-in send will normally cause inter-contract transactions to fail
     function safeSend(address _receiver, uint _amtToSend) private {
-        if (_amtToSend > 0) {
-            bool success = _receiver.send(_amtToSend);
-            if (!success)
-    	        _receiver.call.value(_amtToSend); //May add .gas limit to set a max in case of parasitic contracts, see commented out method below 
-                //_receiver.call.value(_amtToSend).gas(maxGashere);
-        }
+        if (_amtToSend > 0)
+            if (!_receiver.send(_amtToSend))
+        	    _receiver.call.gas(4000000).value(_amtToSend)();
     }
 
     //Checks if two strings are equal by comparing their hashes. Most efficient compare function, when amount of characters isn't large
